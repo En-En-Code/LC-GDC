f2917dffe175b821c2a6625f0cb5f486e95fccc2
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2019-09-04 17:43:43 -0500

    Create project.js

---
+++ Templates/project.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+	document.addEventListener('keydown', keydown);
+	document.addEventListener('keyup', keyup);
+	setInterval(main, 100/6);
+}
+
+function main() {}
