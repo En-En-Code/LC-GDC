475f95ecfd0065826be5571106cf848368a969fc
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2019-09-04 18:34:06 -0500

    Update project.js

--- Templates/project.js
+++ Templates/project.js
@@ -1,6 +1,8 @@
 window.onload = function() {
 	canvas = document.getElementById("canvas");	
 	ctx = canvas.getContext("2d");
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
 	ctx.imageSmoothingEnabled = false;
 	//document.addEventListener('keydown', keydown);
 	//document.addEventListener('keyup', keyup);
