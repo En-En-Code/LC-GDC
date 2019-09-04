26ae3b954dfe1841dc5ee1d0588d992c730ea221
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2019-09-04 18:23:59 -0500

    Update project.js

--- Templates/project.js
+++ Templates/project.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-	canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+	canvas = document.getElementById("canvas");	
+	ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
-	document.addEventListener('keydown', keydown);
-	document.addEventListener('keyup', keyup);
+	//document.addEventListener('keydown', keydown);
+	//document.addEventListener('keyup', keyup);
 	setInterval(main, 100/6);
 }
 
