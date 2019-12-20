79927254420f12cd0d0589ed86e328990c9b3049
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2019-12-20 15:52:56 -0600

    Some small changes
    - Instantiated the Mouse Function (from globals.js)
    - Changed how screen scaling works (more in scenes.js)
    - Added a FightScene to the SceneManager

--- SmashSticks/main.js
+++ SmashSticks/main.js
@@ -7,19 +7,28 @@ var sM = new SceneManager();
 sM.addScene(new StartScene());
 sM.addScene(new OptionsScene());
 sM.addScene(new ControlChangeScene());
+sM.addScene(new FightScene());
 sM.changeScene("start");
 
 window.onload = function() {
 	canvas = document.getElementById("canvas");	
 	ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
+	mouse = new Mouse();
 	setInterval(main, 100/6);
 }
 
 function main() {
 	//These values are now updated every frame, which accommodates changing
-	//the screen size.
-	canvas.width = innerWidth;
-	canvas.height = innerHeight;
+	//the screen size. The SceneManager updates everything inside it in turn.
+	if (canvas.width != innerWidth) {
+		canvas.width = innerWidth;
+		sM.updateXScaling();
+	}
+	if (canvas.height != innerHeight) {
+		canvas.height = innerHeight;
+		sM.updateYScaling();
+	}
+	
 	sM.update();
 }
