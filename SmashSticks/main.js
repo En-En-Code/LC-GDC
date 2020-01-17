e92602681c65c45105c77c5a981a55fe4a296acb
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2020-01-16 20:41:07 -0600

    Added FPS label
    Turn in on with showFPS.value = true; or the button in the options menu

--- SmashSticks/main.js
+++ SmashSticks/main.js
@@ -3,6 +3,9 @@
 	window.onload() function and the initialization of global variables.
 */
 
+var showFPS = {value:false, aveArr:[], point:0, prevTime:Date.now(), currTime:0, fps:0};
+showFPS.aveArr[59] = 0;
+
 var sM = new SceneManager();
 sM.addScene(new StartScene());
 sM.addScene(new OptionsScene());
@@ -11,12 +14,14 @@ sM.addScene(new FightScene());
 sM.changeScene("start");
 
 var gM = new GamepadManager();
+var aM = new SFXManager();
 
 window.onload = function() {
 	canvas = document.getElementById("canvas");	
 	ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
 	mouse = new Mouse();
+	kboard = new Keyboard();
 	document.body.style.cursor = 'none';
 	setInterval(main, 100/6);
 }
@@ -32,9 +37,31 @@ function main() {
 		canvas.height = innerHeight;
 		sM.updateYScaling();
 	}
-	
 	ctx.fillStyle = "#FFFFFF";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	
+	if (showFPS.value) {
+		//determines the running time in between frames to obtain a frame rate
+		///could be better by not checking every frame (e.g. check 1 in 4 frames)
+		showFPS.currTime = Date.now();
+		showFPS.aveArr[showFPS.point] = 1000 / (showFPS.currTime - showFPS.prevTime);
+		showFPS.prevTime = showFPS.currTime;
+		showFPS.point += 1;
+		if (showFPS.point > 59) {
+			showFPS.point = 0;
+			showFPS.fps = 0;
+			for (num of showFPS.aveArr) {
+				showFPS.fps += num;
+			}
+			showFPS.fps /= 60;
+		}
+		ctx.textBaseline = 'middle';
+		ctx.textAlign = 'center';
+		ctx.font = "15px Courier";
+		ctx.fillStyle = "#000000";
+		ctx.fillText(Math.round(showFPS.fps) + " fps", innerWidth/24, innerHeight/24);
+	}
+	
 	sM.update();
 	
 	mouse.update();
