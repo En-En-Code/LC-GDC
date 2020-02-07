9a7ff0deefc79b17c81519f9ca719b901bb93e92
Thewtus <43684926+Thewtus@users.noreply.github.com>
2020-02-07 15:04:12 -0600

    Update character.js
    gamer

--- SmashSticks/character.js
+++ SmashSticks/character.js
@@ -4,14 +4,33 @@
 	the characters and their interactions.
 */
 
-class Character {
-	
+class Character { //will probably just be a collection of animations that the hitboxes is tied to the hitboxes
+	constructor(x, y, w, h){
+		this.keyUp = 68;
+	}
+	update(){
+		if(kboard[this.keyUp]) {
+			this.x += 1;
+		}
+	}
 }
 
 class Hitbox extends Rectangle { //box that determines character's collisions, if it is hit by hurtbox then character takes damage
-	
+	constructor(x, y, w, h, c) {
+		super(x, y, w, h, c)
+	}
+	update() {
+		
+	}
 }
 
 class Hurtbox extends Rectangle { //this is the hitboxes of attacks, if it collides with a hitbox then character takes damage
-	
+	constructor(x, y, w, h) {
+		super(x, y, w, h)
+	}
 }
+
+//arrays and push
+var boxArray = []
+sM.scenes.get("ingame").objs.push(boxArray);
+boxArray.push(testbox);
