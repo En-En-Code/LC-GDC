d361e861058652608981ecb915038fd56dbb3b03
peter-biz <43797290+peter-biz@users.noreply.github.com>
2020-03-06 14:51:51 -0600

    updated character constructer

--- SmashSticks/character.js
+++ SmashSticks/character.js
@@ -5,11 +5,11 @@
 */
 
 class Character extends Rectangle { //will probably just be a collection of animations that the hitboxes is tied to the hitboxes
-	constructor(x, y, w, h, c){
+	constructor(x, y, w, h, c, kr, kl, kj){   //if controls are inversed, just change kr to kl and vice versa
 		super(x, y, w, h, c);
-		this.keyRight = 68;
-		this.keyLeft = 65;
-		this.keyJump = 87;
+		this.keyLeft = kl;
+		this.keyRight = kr;
+		this.keyJump = kj;
 		this.canMove = false;
 		this.gVel = 0;
 		this.G_ACCEL = 1;
@@ -17,10 +17,10 @@ class Character extends Rectangle { //will probably just be a collection of anim
 	update() {
 		if (this.canMove) {
 			if(kboard[this.keyRight]) {
-				this.x += 3;
+				this.x -= 3;
 			}
 			if(kboard[this.keyLeft]) {
-				this.x -= 3;
+				this.x += 3;
 			}
 			if(kboard[this.keyJump]) {
 				this.y -= 15;
@@ -34,12 +34,13 @@ class Hitbox extends Rectangle { //box that determines character's collisions, i
 		super(x, y, w, h, c)
 	}
 	update() {
-		
+
+		//rectRectCollision in global.js
 	}
 }
 
 class Hurtbox extends Rectangle { //this is the hitboxes of attacks, if it collides with a hitbox then character takes damage
 	constructor(x, y, w, h) {
 		super(x, y, w, h)
-	}
+	} 
 }
