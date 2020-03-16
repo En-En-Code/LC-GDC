8abd308962a7aedd6a4cfd369ae0fee33ac2053d
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2020-03-16 11:12:42 -0500

    aState is now set to "WAIT" inside the player

--- SmashSticks/character.js
+++ SmashSticks/character.js
@@ -1,3 +1,9 @@
+/**	SmashSticks/character.js	GDC 2019-2020
+	This file contains code for the playable characters in the game, along
+	with non-playable characters (NPCs), and classes and functions managing
+	the characters and their interactions.
+*/
+
 class Character extends Rectangle { //will probably just be a collection of animations that the hitboxes is tied to the hitboxes
 	constructor(x, y, w, h, c, kl, kr, kj, as = "WAIT") {  
 		super(x, y, w, h, c);
@@ -7,7 +13,7 @@ class Character extends Rectangle { //will probably just be a collection of anim
 		this.canMove = false;
 		this.aState = as;
 		this.gVel = 0;
-		this.G_ACCEL = .7;
+		this.G_ACCEL = 1;
 	}
 	update() {
 		if (this.canMove) {
@@ -18,11 +24,14 @@ class Character extends Rectangle { //will probably just be a collection of anim
 				this.x -= 3;
 			}
 			if(kboard[this.keyJump] && this.aState != "AIR") {
-				this.y -= 25;
+				this.y -= innerHeight/25;
 				this.aState = "AIR-H"; //AIR - HOLDING state - when the player is holding the button, they will continue to rise
 			}
-			if(!kboard[this.keyJump] && this.aState == "AIR")
+			if(!kboard[this.keyJump] && this.aState == "AIR-H")
 				this.aState = "AIR";
+		}
+		if (this.gVel == 0) { //the player has hit the ground
+			this.aState = "WAIT"
 		}
 	}
 }
