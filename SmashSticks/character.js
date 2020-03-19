5a34c042ce1c13804a20bbe015b4acd02f90cacc
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2020-03-19 17:55:46 -0500

    Began Seriously Drawing the Stick Figure Player
    - So this is my second attempt (the first was a while ago and ended in a disaster that made me not want to work on Character at all) at modeling the player. It currently doesn't move or anything special (I'll be getting to that in the future updates), but I wanted some opinions on the scaling, line thickness, etc.
    - Other updates:
      - The player now handles falling in its update function
      - Horizontal movement is scaled to screen size (that was supposed to be part of a previous update, but I forgot somehow)
      - this.aState is now defaulted to "WAIT" in the constructor instead of having its own parameter with a default value of "WAIT"

--- SmashSticks/character.js
+++ SmashSticks/character.js
@@ -4,24 +4,37 @@
 	the characters and their interactions.
 */
 
-class Character extends Rectangle { //will probably just be a collection of animations that the hitboxes is tied to the hitboxes
-	constructor(x, y, w, h, c, kl, kr, kj, as = "WAIT") {  
+class Character extends Rectangle {
+	constructor(x, y, w, h, c, kl, kr, kj) {  
 		super(x, y, w, h, c);
 		this.keyLeft = kl;
 		this.keyRight = kr;
 		this.keyJump = kj;
 		this.canMove = false;
-		this.aState = as;
+		this.aState = "WAIT";
 		this.gVel = 0;
 		this.G_ACCEL = 1;
+		this.nodes = [new Node(this.x, mid2Edge(this.y, this.h) + this.h/32), //head
+					new Node(this.x, this.y), //pelvis
+					new Node(this.x - this.w/4, mid2Edge(this.y, -this.h)), //left foot
+					new Node(this.x + this.w/4, mid2Edge(this.y, -this.h)), //right foot
+					new Node(this.x, this.y - this.h*11/32), //shoulder area
+					new Node(this.x - this.w/4, this.y), //left hand
+					new Node(this.x + this.w/4, this.y)]; //right hand
+		this.connectors = [new Connector(this.nodes[0], this.nodes[4]), //neck
+							new Connector(this.nodes[4], this.nodes[5]), //left arm
+							new Connector(this.nodes[4], this.nodes[6]), //right arm
+							new Connector(this.nodes[4], this.nodes[1]), //torso
+							new Connector(this.nodes[1], this.nodes[2]), //left leg
+							new Connector(this.nodes[1], this.nodes[3])]; //right leg
 	}
 	update() {
 		if (this.canMove) {
 			if(kboard[this.keyRight]) {
-				this.x += 3;
+				this.x += innerWidth/455;
 			}
 			if(kboard[this.keyLeft]) {
-				this.x -= 3;
+				this.x -= innerWidth/455;
 			}
 			if(kboard[this.keyJump] && this.aState != "AIR") {
 				this.y -= innerHeight/25;
@@ -30,13 +43,82 @@ class Character extends Rectangle { //will probably just be a collection of anim
 			if(!kboard[this.keyJump] && this.aState == "AIR-H")
 				this.aState = "AIR";
 		}
-		if (this.gVel == 0) { //the player has hit the ground
+		//Handles gravity
+		this.y += this.gVel;
+		this.gVel += this.G_ACCEL;
+		if (this.gVel <= this.G_ACCEL) { //the player has hit the ground
 			this.aState = "WAIT"
 		}
+
+	}
+	/// These functions are known as "getters" and "setters." You call them without the
+	/// parenthesis (so that it is exactly like requesting the variables.
+	/// Don't worry about this._x & this._y; that is to separate them from this.x & this.y
+	get x() { return this._x; }
+	set x(nX) {
+		if (this.nodes) {
+			for (var n of this.nodes) {
+				n.x += nX - this._x;
+			}
+		}
+		this._x = nX;
+	}
+	get y() {return this._y; }
+	set y(nY) {
+		if (this.nodes) {
+			for (var n of this.nodes) {
+				n.y += nY - this._y;
+			}
+		}
+		this._y = nY;
 	}
 	updateYScaling(o, n) {
 		super.updateYScaling(o, n);
 		this.gVel *= n/o;
 		this.G_ACCEL *= n/o;
+	}
+	render() {
+		super.render();
+		for (var n of this.nodes) {
+			n.render();
+		}
+		for (var c of this.connectors) {
+			c.render();
+		}
+	}
+}
+
+/** CLASSES FOR BUILDING THE CHARACTER **/
+class Node extends Positional {
+	/** A location of interest for stick figure movement, e.g. a foot. **/
+	constructor(x, y) {
+		super(x, y);
+	}
+	update() {
+	}
+	render() {
+		//for testing; will probably be removed
+		ctx.strokeStyle = "#dddd00";
+		ctx.lineWidth = 10;
+		ctx.beginPath();
+		ctx.lineCap = 'round';
+		ctx.moveTo(this.x, this.y);
+		ctx.lineTo(this.x, this.y);
+		ctx.stroke();
+	}
+}
+class Connector {
+	/** A tool for drawing lines between nodes. **/
+	constructor(n1, n2) {
+		this.n1 = n1; this.n2 = n2;
+	}
+	render() {
+		ctx.strokeStyle = "#00dddd";
+		ctx.lineWidth = 8;
+		ctx.beginPath();
+		ctx.lineCap = 'round';
+		ctx.moveTo(this.n1.x, this.n1.y);
+		ctx.lineTo(this.n2.x, this.n2.y);
+		ctx.stroke();
 	}
 }
