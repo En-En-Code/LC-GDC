d72c5a1ba437909bcaa31100ebc8474b4ad8dab8
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2020-03-20 16:39:09 -0500

    Utilized Multithreading to Fix FPS Speed-Up Glitch
    - There are now two functions that control everything instead of one:
      - main() handles everything involving rendering but doesn't update anything. It runs as fast as the computer and/or browser will allow it to.
      - update() handles updating everything (except for a few special cases). It is forced to run at 60 FPS.

--- SmashSticks/main.js
+++ SmashSticks/main.js
@@ -4,7 +4,7 @@
 */
 
 var showFPS = {value:false, aveArr:[], point:0, prevTime:Date.now(), currTime:0, fps:0};
-showFPS.aveArr[59] = 0;
+showFPS.aveArr[29] = 0;
 
 var sM = new SceneManager();
 sM.addScene(new StartScene());
@@ -26,6 +26,7 @@ window.onload = function() {
 	mouse = new Mouse();
 	kboard = new Keyboard();
 	document.body.style.cursor = 'none';
+	setInterval(update, 1000/60);
 	main();
 }
 window.onresize = function() {
@@ -36,8 +37,10 @@ window.onresize = function() {
 }
 
 function main() {
-	sM.update();
-
+	//the main function handles rendering everything; this occurs as fast as your browser can handle
+	sM.render();
+	for (var c of cursorArray) { c.render(); }
+	
 	if (showFPS.value) {
 		//determines the running time in between frames to obtain a frame rate
 		///could be better by not checking every frame (e.g. check 1 in 4 frames)
@@ -45,19 +48,23 @@ function main() {
 		showFPS.aveArr[showFPS.point] = 1000 / (showFPS.currTime - showFPS.prevTime);
 		showFPS.prevTime = showFPS.currTime;
 		showFPS.point += 1;
-		if (showFPS.point > 59) {
+		if (showFPS.point > 29) {
 			showFPS.point = 0;
 			showFPS.fps = 0;
 			for (num of showFPS.aveArr) {
 				showFPS.fps += num;
 			}
-			showFPS.fps /= 60;
+			showFPS.fps /= 30;
 		}
 		formatText(15, "Courier", "#000000", "center", "middle");
 		ctx.fillText(Math.round(showFPS.fps) + " fps", innerWidth/24*devicePixelRatio, innerHeight/24*devicePixelRatio);
 	}
-	
+	requestAnimationFrame(main);
+}
+
+function update() {
+	//the update function handles the updates that occur 60 times per second
+	sM.update();
 	mouse.update();
 	gM.update();
-	requestAnimationFrame(main);
 }
